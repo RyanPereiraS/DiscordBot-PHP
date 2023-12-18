29c62a2bab4f1e9cb3c3176bfa24df40d61a207f
RyanPereiraS <puuppyadr1@gmail.com>
2023-12-18 14:14:57 -0300

    reverted changes

--- events/presence.php
+++ events/presence.php
@@ -1,11 +1,42 @@
 <?php
 
-// Sua instância do Discord
-// Certifique-se de que sua instância do Discord está configurada antes de usar o código abaixo
+use Discord\Discord;
 
 $activityObj = $discord->factory(\Discord\Parts\User\Activity::class);
 $activityObj->type = \Discord\Parts\User\Activity::TYPE_STREAMING;
 $activityObj->name = "Codando em PHP";
 $activityObj->url = "https://twitch.tv/oRyanPereiraS";
 
+$atividades = [
+    ['name' => "Criando Bombas Nucleares", 'url' => "https://twitch.tv/oRyanPereiraS", "type" => \Discord\Parts\User\Activity::TYPE_STREAMING],
+    ['name' => "Codando em PHP", 'url' => "https://twitch.tv/oRyanPereiraS", "type" => \Discord\Parts\User\Activity::TYPE_STREAMING]
+];
+
+foreach ($atividades as $atividade) {
+    global $discord;
+    $atividadeAtual = new stdClass();
+    $atividadeAtual->type = $atividade['type'];
+    $atividadeAtual->name = $atividade['name'];
+    $atividadeAtual->url = $atividade['url'];
+    //updatePresence($atividadeAtual, $discord);
+    //sleep(300);
+}
+
+
+function updatePresences(Discord $discord)
+{
+    //$activityObj = $discord->factory(\Discord\Parts\User\Activity::class);
+    //$activityObj->type = $atividadeAtual->type;
+    //$activityObj->name = $atividadeAtual->name;
+    //$activityObj->url = $atividadeAtual->url;
+    global $activityObj;
+    $discord->updatePresence($activityObj, false, "online", false);
+    echo "Rodou" . PHP_EOL;
+}
+
 $discord->updatePresence($activityObj, false, "online", false);
+/*foreach ($atividades as $atividade) {
+    //global $discord;
+    $discord->updatePresence($activityObj, false, "online", false);
+    //sleep(300);
+}*/
