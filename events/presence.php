f69126e5cd3f0f841ac419a8c14a0c54596fdef9
RyanPereiraS <puuppyadr1@gmail.com>
2023-12-18 14:15:05 -0300

    reverted changes

--- events/presence.php
+++ events/presence.php
@@ -12,31 +12,5 @@ $atividades = [
     ['name' => "Codando em PHP", 'url' => "https://twitch.tv/oRyanPereiraS", "type" => \Discord\Parts\User\Activity::TYPE_STREAMING]
 ];
 
-foreach ($atividades as $atividade) {
-    global $discord;
-    $atividadeAtual = new stdClass();
-    $atividadeAtual->type = $atividade['type'];
-    $atividadeAtual->name = $atividade['name'];
-    $atividadeAtual->url = $atividade['url'];
-    //updatePresence($atividadeAtual, $discord);
-    //sleep(300);
-}
-
-
-function updatePresences(Discord $discord)
-{
-    //$activityObj = $discord->factory(\Discord\Parts\User\Activity::class);
-    //$activityObj->type = $atividadeAtual->type;
-    //$activityObj->name = $atividadeAtual->name;
-    //$activityObj->url = $atividadeAtual->url;
-    global $activityObj;
-    $discord->updatePresence($activityObj, false, "online", false);
-    echo "Rodou" . PHP_EOL;
-}
 
 $discord->updatePresence($activityObj, false, "online", false);
-/*foreach ($atividades as $atividade) {
-    //global $discord;
-    $discord->updatePresence($activityObj, false, "online", false);
-    //sleep(300);
-}*/
